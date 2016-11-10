dbd46b40dd0b3bed331a97a20c58fa8f510fd780
Eli <Eli@Eliana>
2016-11-10 01:49:34 -0300

    n

--- gvgai/src/TestMultiPlayerCeldas.java
+++ gvgai/src/TestMultiPlayerCeldas.java
@@ -25,7 +25,7 @@ public class TestMultiPlayerCeldas
         String agentController = "fiubaceldas.grupo01.Agent";
 
         //definir acá los controladores usados para el juego (se necesitan 2 controladores separados por un espacio)
-        String controllers = doNothingController + " " + agentController;
+        String controllers = agentController + " " + agentController;
         //String controllers = sampleOLMCTSController + " " + sampleOLMCTSController;
 
         //ubicación de los juegos disponibles:
