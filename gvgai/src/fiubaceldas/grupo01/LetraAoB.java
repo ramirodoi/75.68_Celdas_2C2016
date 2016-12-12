6b08da41a783a4386faa498b8305f075740adf53
Eli <Eli@Eliana>
2016-12-11 22:23:53 -0300

    Arreglo nro 8913803

--- gvgai/src/fiubaceldas/grupo01/LetraAoB.java
+++ gvgai/src/fiubaceldas/grupo01/LetraAoB.java
@@ -6,7 +6,8 @@ public class LetraAoB extends Simbolo {
 		this.simbolo = "P";
 	}
 	public boolean incluyeA(Simbolo otroSimbolo) {
-		return ((otroSimbolo.getSimbolo().equals("A")) || (otroSimbolo.getSimbolo().equals("B")));
+		return ((otroSimbolo.getSimbolo().equals("A")) || (otroSimbolo.getSimbolo().equals("B"))
+				|| (otroSimbolo.getSimbolo().equals("Y")) || (otroSimbolo.getSimbolo().equals("Z")) );
 	}
 
 }
